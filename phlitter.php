f403ca516faf6c02c0722d5c6b4189c04a88ba94
Scriptor <historium@gmail.com>
2012-08-28 14:05:40 -0400

    Add support for calling the search api.

--- phlitter.php
+++ phlitter.php
@@ -2,27 +2,52 @@
 require_once('/Users/historium/pharen/lang.php');
 use Pharen\Lexical as Lexical;
 Lexical::$scopes['phlitter'] = array();
-$__scope_id = Lexical::init_closure("phlitter", 126);
-$base_api = "http://api.twitter.com/1/";
-Lexical::bind_lexing("phlitter", 126, '$base_api', $base_api);
-function call_api($action, $params){
-	$base_api = Lexical::get_lexical_binding('phlitter', 126, '$base_api', isset($__closure_id)?$__closure_id:0);;
-	return json_decode(file_get_contents(($base_api . $action . "?" . http_build_query($params))), TRUE);
+$__scope_id = Lexical::init_closure("phlitter", 142);
+$rest_api = "http://api.twitter.com/1/";
+Lexical::bind_lexing("phlitter", 142, '$rest_api', $rest_api);
+$search_api = "http://search.twitter.com/";
+Lexical::bind_lexing("phlitter", 142, '$search_api', $search_api);
+function call_api($type, $action, $params){
+	return json_decode(file_get_contents(($type . $action . "?" . http_build_query($params))), TRUE);
 }
 
-function timeline_for($screen_name, $c){
-	return call_api("statuses/user_timeline.json", array("screen_name" => $screen_name, "count" => $c, "exclute_replies" => $true));
+function call_rest($action, $params){
+	$rest_api = Lexical::get_lexical_binding('phlitter', 142, '$rest_api', isset($__closure_id)?$__closure_id:0);;
+	return call_api($rest_api, $action, $params);
 }
 
-function print_tweet($text){
-	return print(("TWEET: " . $text . "\n\n"));
+function call_search($action, $params){
+	$search_api = Lexical::get_lexical_binding('phlitter', 142, '$search_api', isset($__closure_id)?$__closure_id:0);;
+	return call_api($search_api, $action, $params);
 }
 
-$timeline = timeline_for("tmkhan", 10);
-Lexical::bind_lexing("phlitter", 126, '$timeline', $timeline);
-function phlitter__partial0($arg0){
-	return get("text", $arg0);
+function timeline_for($screen_name, $c=3){
+	return call_rest("statuses/user_timeline.json", array("screen_name" => $screen_name, "count" => $c, "exclude_replies" => TRUE));
 }
 
+function search($query, $result_type="recent", $rpp=5){
+	return call_search("search.json", array("q" => $query, "result_type" => $result_type, "rpp" => $rpp));
+}
 
-map(comp("print_tweet", "\\phlitter__partial0"), $timeline);
+function print_tweet($tweet){
+		return print(("TWEET: " . $tweet["text"] . "\n\n"));
+
+}
+
+function print_tweets($tweets){
+	return map("print_tweet", $tweets);
+}
+
+function show_header($s){
+	prn($s);
+	return prn(str_repeat("=", strlen($s)));
+}
+
+$screen_name = "nasa";
+$timeline = timeline_for($screen_name, 10);
+$__listAcessTmpVar0 = search("#clojure");
+$search_results = $__listAcessTmpVar0["results"];
+show_header("SEARCH RESULTS FOR #CLOJURE");
+print_tweets($search_results);
+show_header("TWEETS BY NASA");
+print_tweets($timeline);
